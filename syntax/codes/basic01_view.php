e2f045ec7e1c3b14a1af6ab6a22c50c2e0b0ef7a
dongww <dongww@gmail.com>
2014-09-06 01:46:23 +0800

    一些小修改

---
+++ syntax/codes/basic01_view.php
@@ -0,0 +1,20 @@
+<!-- basic01_view.php -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>今天的 TODO 列表</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+</head>
+<body>
+
+<p>今天是：<?= today() ?>，今天该做的事情：</p>
+<ul>
+    <?php
+    foreach ($todoList as $time => $event) {
+        echo '<li>', $time, '：', $event, '</li>', PHP_EOL;
+    }
+    ?>
+</ul>
+
+</body>
+</html>
